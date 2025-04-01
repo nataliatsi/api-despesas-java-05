23cf2c2267ad39def80ff8ac5021b6f4ba2573d1
Natália S. Gomes <nataliandsg32@gmail.com>
2025-03-31 21:28:09 -0300

    Correção do repositório local

---
+++ despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/DespesasApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.progirls.despesas.api.despesas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasApiApplication.class, args);
+	}
+
+}
---
+++ despesas-api/src/test/java/com/github/progirls/despesas/api/despesas_api/DespesasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.progirls.despesas.api.despesas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
