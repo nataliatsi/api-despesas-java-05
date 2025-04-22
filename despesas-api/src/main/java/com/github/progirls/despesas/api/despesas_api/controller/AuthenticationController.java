3c217c20041b133ac6061ec5409d27470c3aa4a4
Natália S. Gomes <nataliandsg32@gmail.com>
2025-04-22 11:28:23 -0300

    Adiciona documentação no endpoint de login

--- despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/controller/AuthenticationController.java
+++ despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/controller/AuthenticationController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.progirls.despesas.api.despesas_api.security.AuthenticationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+
 @RestController
 @RequestMapping("/api/v1/login")
 public class AuthenticationController {
@@ -22,10 +25,14 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-    @PostMapping
+    @Operation(summary = "Autenticar usuário e gerar token", description = "Realiza a autenticação do usuário com credenciais básicas e retorna um token JWT válido para acesso às demais rotas protegidas da API.", responses = {
+            @ApiResponse(responseCode = "200", description = "Autenticação bem-sucedida. Token retornado com sucesso."),
+            @ApiResponse(responseCode = "401", description = "Falha na autenticação. Credenciais inválidas ou ausentes.")
+    })
     @SecurityRequirement(name = "basicAuth")
+    @PostMapping
     public ResponseEntity<?> authenticate(Authentication authentication) {
-        String token = authenticationService.autheticate(authentication);
+        String token = authenticationService.authenticate(authentication);
         return ResponseEntity.ok(Map.of("token", token));
     }
 
