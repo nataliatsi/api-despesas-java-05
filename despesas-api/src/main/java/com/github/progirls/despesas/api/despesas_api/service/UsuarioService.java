4838e4c20947236888d652181b92b5f506d3f599
Natália S. Gomes <nataliandsg32@gmail.com>
2025-04-22 13:49:55 -0300

    Adiciona funcionalidade de redefinição de senha com autenticação

--- despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/service/UsuarioService.java
+++ despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/service/UsuarioService.java
@@ -3,17 +3,21 @@ package com.github.progirls.despesas.api.despesas_api.service;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.github.progirls.despesas.api.despesas_api.dto.UsuarioRedefinirSenhaDTO;
 import com.github.progirls.despesas.api.despesas_api.dto.UsuarioRegisterDto;
 import com.github.progirls.despesas.api.despesas_api.entities.Usuario;
 import com.github.progirls.despesas.api.despesas_api.repository.UsuarioRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UsuarioService {
-    
+
     private BCryptPasswordEncoder passwordEncoder;
     private UsuarioRepository usuarioRepository;
 
@@ -30,7 +34,7 @@ public class UsuarioService {
         if (usuarioEmail.isPresent()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Email já cadastrado, por favor tente outro.");
         }
-        
+
         String encryptedPassword = passwordEncoder.encode(usuarioRegisterDto.senha());
 
         // Criando o novo usuário e salvando os parâmetros passados
@@ -44,5 +48,18 @@ public class UsuarioService {
         usuario = usuarioRepository.save(usuario);
 
         return usuario;
+    }
+
+    @Transactional
+    public void atualizarSenha(UsuarioRedefinirSenhaDTO novaSenha, Authentication authentication) {
+
+        String emailUsuario = authentication.getName();
+        Usuario usuario = usuarioRepository.findByEmail(emailUsuario)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        String novaSenhaCriptografada = passwordEncoder.encode(novaSenha.senha());
+
+        usuario.setSenha(novaSenhaCriptografada);
+        usuarioRepository.save(usuario);
     }
 }
