cf899f1a3f4542ff74f0fe11af706015701016b7
Natália S. Gomes <nataliandsg32@gmail.com>
2025-04-28 13:10:35 -0300

    Ativar perfil de 'test' para classe principal dos testes

--- despesas-api/src/test/java/com/github/progirls/despesas/api/despesas_api/DespesasApiApplicationTests.java
+++ despesas-api/src/test/java/com/github/progirls/despesas/api/despesas_api/DespesasApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.progirls.despesas.api.despesas_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DespesasApiApplicationTests {
 
 	@Test
