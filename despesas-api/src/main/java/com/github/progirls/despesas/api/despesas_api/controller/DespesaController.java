9951e53140d4918d73fdc314939edbf2347e7c21
Milena <milena181903@hotmail.com>
2025-05-05 18:51:27 -0300

    Adicionado os testes da despesa

--- despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/controller/DespesaController.java
+++ despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/controller/DespesaController.java
@@ -2,6 +2,8 @@ package com.github.progirls.despesas.api.despesas_api.controller;
 
 import com.github.progirls.despesas.api.despesas_api.dto.NovaDespesaDTO;
 import com.github.progirls.despesas.api.despesas_api.service.DespesaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +23,23 @@ public class DespesaController {
         this.despesaService = despesaService;
     }
 
+    @Operation(summary = "Criar uma nova despesa ao usuário autenticado",
+
+            description = "Este endpoint permite que o usuário crie uma despesa nova. " +
+                    "O usuário precisa estar autenticado para realizar a criação. ", responses = {
+            @ApiResponse(responseCode = "201", description = "Despesa criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Despesa com dados incompletos ou inválidos."),
+            @ApiResponse(responseCode = "401", description = "Falha na autenticação. Token inválido ou ausente.")})
     @PostMapping
     public ResponseEntity<?> criarDespesa(@Valid @RequestBody NovaDespesaDTO dto, Authentication authentication) {
-        var novaDespesa = despesaService.criarDespesa(dto, authentication);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novaDespesa);
+        try {
+            var novaDespesa = despesaService.criarDespesa(dto, authentication);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Despesa Criada com sucesso " + novaDespesa);
+
+        } catch (Exception e) {
+
+            return ResponseEntity.badRequest().body("Erro inesperado: "  +e.getMessage());
+        }
 
     }
 }
