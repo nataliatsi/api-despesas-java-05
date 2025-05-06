0c7c16414299caee695dcde68309edbca61cf0f0
Natália S. Gomes <nataliandsg32@gmail.com>
2025-05-06 15:06:02 -0300

    refactor: Ajuste do repositório para utilizar JpaSpecificationExecutor.

--- despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/repository/DespesaRepository.java
+++ despesas-api/src/main/java/com/github/progirls/despesas/api/despesas_api/repository/DespesaRepository.java
@@ -2,6 +2,7 @@ package com.github.progirls.despesas.api.despesas_api.repository;
 
 import com.github.progirls.despesas.api.despesas_api.entities.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DespesaRepository extends JpaRepository<Despesa, Long> {
+public interface DespesaRepository extends JpaRepository<Despesa, Long>, JpaSpecificationExecutor<Despesa> {
 }
